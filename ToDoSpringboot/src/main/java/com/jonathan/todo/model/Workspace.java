b33dc564022265f1a28a5ad831c0c4311eced892
JonathanCharles2003 <jb2366@srmist.edu.in>
2025-11-01 23:24:16 +0530

    Added the entity class Workspace

---
+++ ToDoSpringboot/src/main/java/com/jonathan/todo/model/Workspace.java
@@ -0,0 +1,108 @@
+package com.jonathan.todo.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "workspace")
+@EntityListeners(AuditingEntityListener.class)
+public class Workspace {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long workspaceId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@Column(nullable = false, unique = true, length = 100)
+	private String workspaceName;
+	
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+	
+	
+	private LocalDateTime deletedAt;
+
+
+	public Long getWorkspaceId() {
+		return workspaceId;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public String getWorkspaceName() {
+		return workspaceName;
+	}
+
+
+	public void setWorkspaceName(String workspaceName) {
+		this.workspaceName = workspaceName;
+	}
+
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+	public LocalDateTime getDeletedAt() {
+		return deletedAt;
+	}
+
+
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+
+	public void setWorkspaceId(Long workspaceId) {
+		this.workspaceId = workspaceId;
+	}
+	
+	
+	
+	
+}
